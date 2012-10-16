0c2361060eb4682064fa02188b084a04dc0008ce
clydeli <clyde1008li@gmail.com>
2012-10-15 22:23:27 -0700

    api_wrapper

---
+++ public/geotag_new/api_wrapper.js
@@ -0,0 +1,46 @@
+
+function sd_create (type, post_data){
+  
+  $.ajax({
+      type: 'POST', url: "../"+type+".json",
+      data: post_data, dataType: 'json',
+      success: function(rcv_data){ 
+        console.log("received: "+rcv_data);
+        return rcv_data;
+      }
+  });
+
+}
+ 
+ /*       
+        $(".btns_create").click(function() {
+          var post_data = { label: $("#person_label").val() }         
+          $.ajax({
+            type: 'POST', url: "../people.json",
+            data: post_data, dataType: 'json',
+            success: function(rcv_guid){ 
+              console.log(rcv_guid);
+            }
+          });
+        });
+
+        $("#btn_create_person").click(function() {
+          var post_data = { label: $("#person_label").val() }         
+          $.ajax({
+            type: 'POST', url: "../people.json",
+            data: post_data, dataType: 'json',
+            success: function(rcv_guid){ 
+              console.log(rcv_guid);
+            }
+          });
+        });
+        $("#btn_create_event").click(function() {      
+          $.ajax({
+            type: 'POST', url: "../events.json",
+            data: { label: $("#event_label").val(), dateTime: $("#event_datetime").val() }, dataType: 'json',
+            success: function(rcv_guid){ 
+              console.log(rcv_guid);
+            }
+          });
+        });
+  */
