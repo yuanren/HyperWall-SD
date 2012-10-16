afdc01566239678ab6c0ece1c260616e6a35aba8
clydeli <clyde1008li@gmail.com>
2012-10-15 23:23:49 -0700

    api_wrapper_7

--- public/geotag_new/api_wrapper.js
+++ public/geotag_new/api_wrapper.js
@@ -2,7 +2,7 @@
 function sd_create (type, post_data){
   $.ajax({
       type: 'POST', url: "../"+type+".json",
-      data: post_data, dataType: 'json',
+      data: post_data, dataType: 'json', async: false,
       success: function(rcv_data){ 
         console.log(rcv_data);
         return rcv_data['GUID'];
@@ -14,7 +14,7 @@ function sd_get (type, get_data){
   //var rcv_guid;
   $.ajax({
       type: 'GET', url: "../get_"+type,
-      data: get_data, dataType: 'json',
+      data: get_data, dataType: 'json', async: false,
       success: function(rcv_data){ 
         console.log(rcv_data);
       }
