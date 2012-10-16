d299a0d0ee0ff25745e6935c7831be827f98db0b
clydeli <clyde1008li@gmail.com>
2012-10-16 16:22:32 -0700

    api_wrapper_9

--- public/geotag_new/api_wrapper.js
+++ public/geotag_new/api_wrapper.js
@@ -5,13 +5,12 @@ function sd_create (type, post_data){
       data: post_data, dataType: 'json', async: false,
       success: function(rcv_data){ 
         console.log(rcv_data);
-        return rcv_data['GUID'];
+        //return rcv_data['GUID'];
       }
   });
 }
 
 function sd_get (type, get_data){
-  //var rcv_guid;
   $.ajax({
       type: 'GET', url: "../get_"+type,
       data: get_data, dataType: 'json', async: false,
