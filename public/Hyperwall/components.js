7fb803183f10bb5fecfba3269befebffbd8e3d25
clydeli <clyde1008li@gmail.com>
2012-10-17 16:18:56 -0700

    elements adding 7

--- public/Hyperwall/components.js
+++ public/Hyperwall/components.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   console.log($("body").width()-200);
-  $("#critical_list").css("left", ($("html").width()-204)+"px");
-  $("#tracked_user_list").css("top", ($("html").height()-$(this).height()-24)+"px");
+  $("#critical_list").css("left", ($(document).width()-204)+"px");
+  $("#tracked_user_list").css("top", ($(document).height()-$(this).height()-24)+"px");
 
 });
