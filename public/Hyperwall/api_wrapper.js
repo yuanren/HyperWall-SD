be062fd829a6cd2985b0233d01f552155a232510
clydeli <clyde1008li@gmail.com>
2012-10-16 23:40:44 -0700

    main basic structure fix 7

--- public/Hyperwall/api_wrapper.js
+++ public/Hyperwall/api_wrapper.js
@@ -2,7 +2,7 @@
 function sd_create (type, post_data, callback_function){
   $.ajax({
       type: 'POST', url: "../"+type+".json",
-      data: post_data, dataType: 'json'
+      data: post_data, dataType: 'json',
       success: function(rcv_data){ 
         callback_function(rcv_data);
         //return rcv_data['GUID'];
@@ -13,7 +13,7 @@ function sd_create (type, post_data, callback_function){
 function sd_get (type, get_data){
   $.ajax({
       type: 'GET', url: "../get_"+type,
-      data: get_data, dataType: 'json'/*, async: false,*/
+      data: get_data, dataType: 'json',/* async: false,*/
       success: function(rcv_data){ 
         console.log(rcv_data);
       }
