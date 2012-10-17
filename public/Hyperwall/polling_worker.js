291330f535289ac0935b14a3bf4832db2bba5249
clydeli <clyde1008li@gmail.com>
2012-10-17 14:24:32 -0700

    add polling worker thread 11

--- public/Hyperwall/polling_worker.js
+++ public/Hyperwall/polling_worker.js
@@ -10,6 +10,19 @@ function poll_conversations(){
   });*/
   try {
     var xhr=new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        if (xhr.status == 200 || xhr.status ==0) { postMessage(xhr.responseText); }
+        else { throw  xhr.status+xhr.responseText; }
+        setTimeout(poll_conversations(), 5000);  
+	  }
+	};
+    xhr.open("GET","../get_guid?type=Event",true);
+    xhr.send();
+  } catch(e){ postMessage("ERROR:"+e.message); }
+
+
+/*
     xhr.onreadystatechange=function()
   {
   if (xhr.readyState==4 && xhr.status==200)
@@ -21,6 +34,7 @@ function poll_conversations(){
   xhr.open("GET","../get_guid?type=Event",true);
   xhr.send();
   } catch (e) { postMessage("ERROR:"+e.message); }
+*/
 }
 
 self.addEventListener('message', function(e) {
