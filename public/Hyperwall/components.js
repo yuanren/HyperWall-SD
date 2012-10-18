4b16cd3cd3cde96fa01cec0b4cacc75ef3d630ba
clydeli <clyde1008li@gmail.com>
2012-10-17 17:30:22 -0700

    poll test 2

--- public/Hyperwall/components.js
+++ public/Hyperwall/components.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  console.log($("body").width()-200);
   $("#critical_list").css("left", ($("body").width()-204)+"px");
   $("#tracked_user_list").css("top", ($("body").height()-124)+"px");
 
