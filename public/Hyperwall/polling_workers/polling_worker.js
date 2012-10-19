ef4199bf376ef07172f1bbbd34bde4d74941641f
clydeli <clyde1008li@gmail.com>
2012-10-18 17:58:40 -0700

    polling change 8

--- public/Hyperwall/polling_workers/polling_worker.js
+++ public/Hyperwall/polling_workers/polling_worker.js
@@ -7,7 +7,7 @@ function poll_conversations(){
       if (xhr.readyState == 4) {
         if (xhr.status == 200 || xhr.status ==0) { postMessage(xhr.responseText); }
         else { throw  xhr.status+xhr.responseText; }
-        setTimeout(poll_conversations(), 5000);
+        //setTimeout(poll_conversations(), 5000);
 	    }
 	  };
     xhr.open("GET","../../get_guid?type=Event",true);
