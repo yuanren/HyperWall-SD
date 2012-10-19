672d614c5dcc37b31fb4a3a7bb2237637063663a
clydeli <clyde1008li@gmail.com>
2012-10-19 00:51:38 -0700

    mockup test 6

--- public/Hyperwall/components.js
+++ public/Hyperwall/components.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $("#critical_list").css("left", ($("body").width()-224)+"px");
   $("#tracked_user_list").css("top", ($("body").height()-124)+"px");
 
+  $(".inmap_dialog").parent().parent().parent().css('box-shadow', '1px 1px 20px 1px #900');
 });
+
+
