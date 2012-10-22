f9d091fa644585142975f1459249f8086f7bd0a5
Yuan Ren <yuan.ren@sv.cmu.edu>
2012-10-22 12:16:29 -0700

    fixes in IdTableNameController

--- app/controllers/id_table_name_controller.rb
+++ app/controllers/id_table_name_controller.rb
@@ -3,6 +3,17 @@ class IdTableNameController < ApplicationController
     @association_id = UUIDTools::UUID.timestamp_create.to_s
     @result = "Success"
     params[:objects].each do |objectId|
+      @idTableName = IdTableName.find_by_id(objectId)
+      if @idTableName.nil?
+        @result = "Fail. id #{objectId} is not found."
+        respond_to do |format|
+          format.json { render json: {:result => @result} }
+        end
+        return
+      end
+      if @idTableName.tableName == "Place"
+        @place = Place.find_by_placeId(objectId)
+      end
       @immutable_association = ImmutableAssociation.new
       @immutable_association.associationId = @association_id
       @immutable_association.objectId = objectId
@@ -11,71 +22,27 @@ class IdTableNameController < ApplicationController
         break
       end
     end
+    if !@place.nil?
+      # associate place with message if any
+      params[:objects].each do |objectId|
+        @idTableName = IdTableName.find_by_id(objectId)
+        # this is only to associate a Message's or an Image's place. To associate place for Person or Thing use add_breadcrumb
+        if @idTableName.tableName == "Message" or @idTableName.tableName == "Image"
+          @resourcePlace = ResourcePlace.new
+          @resourcePlace.resourceId = objectId
+          @resourcePlace.placeId = @place.placeId
+          @resourcePlace.dateTime = Time.now
+          @resourcePlace.version = 1
+          if @resourcePlace.save
+            @result = "Success"
+          else
+            @result = "Fail. Couldn't associate #{@idTableName.tableName} #{objectId} with Place #{@place.placeId}."
+          end
+        end
+      end
+    end
     respond_to do |format|
       format.json { render json: {:result => @result} }
-    end
-  end
-
-  def associate_guids_old
-    @sourceIdTableName = IdTableName.find(params[:sourceGUID])
-    @sourceTableName = @sourceIdTableName.tableName
-
-    @destIdTableName = IdTableName.find(params[:destinationGUID])
-    @destTableName = @destIdTableName.tableName
-
-    @result = "fail"
-    if ((@sourceTableName == "Event" and @destTableName == "Person") or
-        (@sourceTableName == "Person" and @destTableName == "Event") or
-        (@sourceTableName == "Event" and @destTableName == "Conversation") or
-        (@sourceTableName == "Conversation" and @destTableName == "Event") or
-        (@sourceTableName == "Event" and @destTableName == "Message") or
-        (@sourceTableName == "Message" and @destTableName == "Event") or
-        (@sourceTableName == "Event" and @destTableName == "Thing") or
-        (@sourceTableName == "Thing" and @destTableName == "Event") or
-        (@sourceTableName == "Event" and @destTableName == "Image") or
-        (@sourceTableName == "Image" and @destTableName == "Event")
-      )
-      @source = Event.find(params[:sourceGUID])
-      @dest = Person.find(params[:destinationGUID])
-      @eventResource = EventResource.new
-      @eventResource.eventId = @source.eventId
-      @eventResource.resourceId = @dest.resourceId
-      if @eventResource.save
-        @result = "success"
-      end
-    elsif ((@sourceTableName == "Event" and @destTableName == "Place") or
-        (@sourceTableName == "Place" and @destTableName == "Event"))
-      @source = Event.find(params[:sourceGUID])
-      @dest = Place.find(params[:destinationGUID])
-      @eventPlace = EventPlace.new
-      @eventPlace.eventId = @source.eventId
-      @eventPlace.placeId = @dest.placeId
-      if @eventPlace.save
-        @result = "success"
-      end
-    elsif (
-        (@sourceTableName == "Place" and @destTableName == "Person") or
-        (@sourceTableName == "Person" and @destTableName == "Place") or
-        (@sourceTableName == "Place" and @destTableName == "Conversation") or
-        (@sourceTableName == "Conversation" and @destTableName == "Place") or
-        (@sourceTableName == "Place" and @destTableName == "Message") or
-        (@sourceTableName == "Message" and @destTableName == "Place") or
-        (@sourceTableName == "Place" and @destTableName == "Thing") or
-        (@sourceTableName == "Thing" and @destTableName == "Place") or
-        (@sourceTableName == "Place" and @destTableName == "Image") or
-        (@sourceTableName == "Image" and @destTableName == "Place")
-        )
-      @source = Resource.find(params[:sourceGUID])
-      @dest = Place.find(params[:destinationGUID])
-      @resourcePlace = ResourcePlace.new
-      @resourcePlace.resourceId = @source.eventId
-      @resourcePlace.placeId = @dest.placeId
-      if @resourcePlace.save
-        @result = "success"
-      end
-    end
-    respond_to do |format|
-      format.json { render :json => {:result => @result} }
     end
   end
 
@@ -110,7 +77,7 @@ class IdTableNameController < ApplicationController
     if @depth >= 1
       # if object is a message, include its conversation in result and vice versa
       if @tableName == "Conversation" and @type.nil? or @type == "Message"
-        @associatedObjects << ["Message", @object.message]
+        @associatedObjects << ["Message", @object.message.sort{|a,b| b.dateTime <=> a.dateTime}]
         # association from conversation to message is considered as one hop
         @depth -= 1
       end
@@ -130,7 +97,7 @@ class IdTableNameController < ApplicationController
           @objectAssociations = ImmutableAssociation.find_all_by_associationId(association.associationId)
           @objectAssociations.each do |objectAssociation|
             @objectId = objectAssociation.objectId
-            @associatedTableName = IdTableName.find_last_by_objectId(@objectId).tableName
+            @associatedTableName = IdTableName.find_last_by_id(@objectId).tableName
             if @type.nil? or @type == @associatedTableName
               @associatedObjectsPerAssociation << [@associatedTableName, @associatedTableName.constantize.find(@objectId)]
             end
@@ -329,27 +296,31 @@ class IdTableNameController < ApplicationController
           next if @object.nil?
           @text = @object.label
       end
-      if @text.nil? or params[:valueRange].nil? or @text =~ /#{params[:valueRange]}/
-        case idTableName.tableName
-          when "Event"
-            @eventPlace = EventPlace.find_last_by_eventId(@id)
-            @place = Place.find_by_placeId(@eventPlace.placeId) if !@eventPlace.nil?
-          when "Person", "Thing", "Image", "Message", "Conversation"
-            @resourcePlace = ResourcePlace.find_last_by_resourceId(@id)
-            @place = Place.find_by_placeId(@resourcePlace.placeId) if !@resourcePlace.nil?
-          when "Place"
-            @place = Place.find_by_placeId(@id)
-        end
-        if (@minLat != -1 or @minLon != -1) and !@place.nil?
-          if (@minLat == -1 or (@minLat..@maxLat).cover?(@place.latitude)) and
-              (@minLon == -1 or (@minLon..@maxLon).cover?(@place.longitude))
-            @resultObjects << @object
-          end
-        else
+
+      if !params[:valueRange].nil?
+        next if @text.nil? or !(@text =~ /#{params[:valueRange]}/)
+      end
+
+      case idTableName.tableName
+        when "Event"
+          @eventPlace = EventPlace.find_last_by_eventId(@id)
+          @place = Place.find_by_placeId(@eventPlace.placeId) if !@eventPlace.nil?
+        when "Person", "Thing", "Image", "Message", "Conversation"
+          @resourcePlace = ResourcePlace.find_last_by_resourceId(@id)
+          @place = Place.find_by_placeId(@resourcePlace.placeId) if !@resourcePlace.nil?
+        when "Place"
+          @place = Place.find_by_placeId(@id)
+      end
+      if (@minLat != -1 or @minLon != -1) and !@place.nil?
+        if (@minLat == -1 or (@minLat..@maxLat).cover?(@place.latitude)) and
+            (@minLon == -1 or (@minLon..@maxLon).cover?(@place.longitude))
           @resultObjects << @object
         end
+      else
+        @resultObjects << @object
       end
     end
+
     respond_to do |format|
       format.json { render :json => {:objects => @resultObjects}}
     end
