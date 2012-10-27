17cbf9b0d97dd3f74bad6b3be75f8a1e9fe80d4f
clydeli <clyde1008li@gmail.com>
2012-10-27 01:17:40 -0700

    read unread 2

--- public/Hyperwall/page.js
+++ public/Hyperwall/page.js
@@ -299,7 +299,7 @@ function initialize() {
 
   // General list item triggers
   $("#general_list, #critical_list").on("click", ".list_msg", function(event){
-    $(this).css("opacity", "0.4");
+    $(this).fadeTo("slow", 0.45);
     var conversation_guid = $(this).find(".conversation_guid").val();
     if(CONVERSATION_HASH[conversation_guid].hasOwnProperty("MAP_MARKER")){
       MAP.setZoom(17);
