26d8b6ffccfd18f8a40eef56cfa2708d04bb1a4d
Yuan Ren <yuan.ren@sv.cmu.edu>
2012-10-29 09:53:37 -0700

    added curl tests

--- test/curl_test/test.sh
+++ test/curl_test/test.sh
@@ -4,13 +4,29 @@ curl -H "Content-Type: application/json" --data @post_person.json "http://localh
 
 curl -H "Content-Type: application/json" --data @post_conversation.json "http://localhost:3000/conversations.json"
 
+curl -H "Content-Type: application/json" --data @post_image.json "http://localhost:3000/images.json"
+
+curl -H "Content-Type: application/json" --data @post_image_2.json "http://localhost:3000/images.json"
+
 curl -H "Content-Type: application/json" --data @post_message.json "http://localhost:3000/messages.json"
 
 curl -H "Content-Type: application/json" --data @post_place.json "http://localhost:3000/places.json"
 
 curl -H "Content-Type: application/json" --data @get_property.json "http://localhost:3000/get_properties"
 
+curl -H "Content-Type: application/json" --data @get_property_2.json "http://localhost:3000/get_properties"
+
 curl -H "Content-Type: application/json" --data @get_guid.json "http://localhost:3000/get_guid"
 
+curl -H "Content-Type: application/json" --data @get_objects.json "http://localhost:3000/get_objects"
+
 curl -H "Content-Type: application/json" --data @associate_guids.json "http://localhost:3000/associate_guids"
+
+curl -H "Content-Type: application/json" --data @add_breadcrumb.json "http://localhost:3000/add_breadcrumb"
+
+curl -H "Content-Type: application/json" --data @get_breadcrumbs.json "http://localhost:3000/get_breadcrumbs"
+
+curl -H "Content-Type: application/json" --data @add_escalation.json "http://localhost:3000/escalations.json"
+
+curl -H "Content-Type: application/json" --data @get_escalated_objects.json "http://localhost:3000/get_escalated_objects"
 
---
+++ test/unit/escalation_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EscalationTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
