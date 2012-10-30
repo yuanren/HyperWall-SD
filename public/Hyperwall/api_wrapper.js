9bc818629e0f9b3b8630c0de50d7504178646221
clydeli <clyde1008li@gmail.com>
2012-10-30 01:16:53 -0700

    breadcrumb prepare 9

--- public/Hyperwall/api_wrapper.js
+++ public/Hyperwall/api_wrapper.js
@@ -17,11 +17,19 @@ function sd_get (type, get_data, callback_function){
  
 // Shorthanded Google Map API calls
 function gm_create_marker(type, location) {
-  //switch(type)
+  var icon_type;
+  switch(type){
+    case "breadcrumb":
+      icon_type = "https://maps.gstatic.com/intl/en_us/mapfiles/markers2/measle.png";
+      break;
+    default:
+      icon_type = "http://maps.google.com/mapfiles/marker.png";
+      break;
+  }
   var marker = new google.maps.Marker({
     position: new google.maps.LatLng( location[0], location[1]),
     map: MAP,
-    icon: "http://maps.google.com/mapfiles/marker.png"
+    icon: icon_type
   });
   return marker;
 }
